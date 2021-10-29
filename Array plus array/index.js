63653842c290fea1e77e8fa4bfc9af7a33d1a837
BHOPAL SINGH <bhopalsinghsis@gmail.com>
2021-10-29 13:24:56 -0400

    Update index.js

--- Array plus array/index.js
+++ Array plus array/index.js
@@ -3,3 +3,9 @@
 function arrayPlusArray(arr1, arr2) {
   return arr1.concat(arr2).reduce((prev, curr) => prev + curr)
 }
+
+
+// accepts indefinite number of arrays
+function arrayPlusArray(...arrays) {
+  return [].concat(...arrays).reduce((a,b) => a+b,0)
+}
